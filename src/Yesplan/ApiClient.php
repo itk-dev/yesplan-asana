c1c91c2c8b7d57786a7b72534c6b89ae72b05b5e
Line Stiil <lilosti@aarhus.dk>
2020-04-16 13:18:02 +0200

    MUHU:120, install http-client, get events from yesplan

--- src/Yesplan/ApiClient.php
+++ src/Yesplan/ApiClient.php
@@ -2,11 +2,36 @@
 
 namespace App\Yesplan;
 
+use Symfony\Component\HttpClient\CurlHttpClient;
+use Symfony\Component\HttpClient\NativeHttpClient;
+
 class ApiClient{
     public function getEvents():array
     {
-        return [
-            ['id'=>'efg456', 'title'=>'test']
-        ];
+        $client = new CurlHttpClient();
+        $response = $client->request('GET', 'https://musikhusetaarhus.yesplan.be/api/events?api_key=38102428C8331E40E3D65E867DE96B1A');
+        $responseJson = json_decode($response->getContent(), true);
+        $id;
+        $eventArray = array();
+        foreach($responseJson as $data){
+            if(is_array($data)){
+                foreach($data as $dataValues){
+                    if(is_array($dataValues)){
+                       $id = $dataValues["id"];
+                       $eventArray[$id]['id'] = $id;  
+                       $eventArray[$id]['data'] = $dataValues; 
+                    }
+
+                }
+            }
+        }
+
+ //      $id = $responseJson["data"]["0"]["id"];
+
+        return 
+                      $eventArray;
+                   
+            
+
     }
 }
