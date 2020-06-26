5306742ea13010fc769e8de8d31a9f1167148c0c
Line Stiil <lilosti@aarhus.dk>
2020-06-26 07:44:47 +0200

    MUHU-126: added new fields to yesplanEvent

---
+++ src/Migrations/Version20200626054104.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200626054104 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE yesplan_event ADD status VARCHAR(255) DEFAULT NULL, ADD status_id VARCHAR(255) DEFAULT NULL, ADD profile VARCHAR(255) DEFAULT NULL, ADD profile_id VARCHAR(255) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE yesplan_event DROP status, DROP status_id, DROP profile, DROP profile_id');
+    }
+}
