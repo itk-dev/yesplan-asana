d2a579a7d2c77ad8b3505c61940203fc53d5deaa
Line Stiil <lilosti@aarhus.dk>
2020-10-02 07:56:52 +0200

    MUHU-169: applied coding standards, and removed debug output

--- src/Asana/AsanaEventManager.php
+++ src/Asana/AsanaEventManager.php
@@ -50,7 +50,7 @@ class AsanaEventManager
         $eventsNewEventsExternal = $this->eventRepository->findNewProductionOnlineIncludingGratisandExternEvents();
 
         //create the cards, and update asanaEvent table
-        
+
         foreach ($lastMinutEvents as $lastMinuteEvent) {
             $eventData = $this->getEventData($this->eventRepository->find($lastMinuteEvent['id']));
             $this->asanaApiClient->createCardLastMinute($eventData);
@@ -74,7 +74,7 @@ class AsanaEventManager
             $this->asanaApiClient->createCardNewEventsBoard($eventData);
             $this->cardCreated($eventsNewEvent['id'], self::EVENTS);
         }
-        
+
         foreach ($eventsNewEventsExternal as $eventsNewEventExternal) {
             $eventData = $this->getEventData($this->eventRepository->find($eventsNewEventExternal['id']));
             $this->asanaApiClient->createCardNewEventsGratisandExternBoard($eventData);
