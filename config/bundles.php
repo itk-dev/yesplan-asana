beac82051f8370d4ca54e01115eef5f39a277c1e
Martin Kristiansen <n1tramkr1st1ansen@gmail.com>
2023-02-15 14:36:50 +0100

    MUHU-190: created new project, added files, installed makerbundle for entity integration;

--- config/bundles.php
+++ config/bundles.php
@@ -2,9 +2,7 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
-    Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
-    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
 ];
